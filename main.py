3352dea1d79f2b19871d90af22f0ef57ac915312
Monika Georgieva <monikageorgieva950@gmail.com>
2023-05-19 21:05:10 +0300

    movement and frame

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ class App:
         pg.display.set_caption('Tetris')
         self.screen = pg.display.set_mode(FIELD_RES)
         self.clock = pg.time.Clock()
+        self.set_timer()
         self.tetris = Tetris(self)
+
+    def set_timer(self):
+        self.user_event = pg.USEREVENT+0
+        self.anim_trigger = False
+        pg.time.set_timer(self.user_event,ANIM_TIME_INTERVAL)
+
 
     def update(self):
         self.tetris.update()
-        self.clock.tick(2)
+        self.clock.tick(FPS)
 
     def draw(self):
         self.screen.fill(color=FIELD_COLOR)
@@ -21,10 +28,15 @@ class App:
         pg.display.flip()
 
     def check_events(self):
+        self.anim_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            elif event.type == pg.KEYDOWN:
+                self.tetris.control(pressed_key=event.key)
+            elif event.type == self.user_event:
+                self.anim_trigger = True
 
     def run(self):
         while True:
--- tetris.py
+++ tetris.py
@@ -8,14 +8,28 @@ class Tetris:
         self.app = app
         self.sprite_group = pg.sprite.Group()
         self.tetromino = Tetromino(self)
+    
+    def check_tetromino_landing(self):
+        if self.tetromino.landing:
+            self.tetromino = Tetromino(self)
+
+    def control(self,pressed_key):
+        if pressed_key == pg.K_LEFT:
+            self.tetromino.move(direction="right")
+        elif pressed_key == pg.K_RIGHT:
+            self.tetromino.move(direction="left")
 
     def draw_grid(self):
         for i in range(FIELD_W):
             for j in range(FIELD_H):
                 pg.draw.rect(self.app.screen,"black",(i*TILE_SIZE,j*TILE_SIZE,TILE_SIZE,TILE_SIZE),1)
+
     def update(self):
-        self.tetromino.update()
+        if self.app.anim_trigger:
+            self.tetromino.update()
+            self.check_tetromino_landing()
         self.sprite_group.update()
+
     def draw(self):
         self.draw_grid()
         self.sprite_group.draw(self.app.screen)
